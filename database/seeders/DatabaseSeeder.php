30509bd581c3d7f6a2a85c431b64b4d2eda7e321
John Doe <johndoe@example.com>
2025-03-07 10:26:22 +0100

    Seeding backend finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $categories = Category::factory(10)->create();
+        User::factory(5)
+            ->has(
+                Product::factory(10)->state(function() use ($categories) {
+                    return ['category_id' => $categories->random()->id];
+                })
+            )
+            ->create();
     }
 }
